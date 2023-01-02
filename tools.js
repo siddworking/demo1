326d91a349b5a96c75da5a8e5f5bf97ee0f4249d
siddworking <nandsiddhant7@gmail.com>
2023-01-02 17:39:10 +0530

    upates

---
+++ tools.js
@@ -0,0 +1,22 @@
+
+/*
+sudo yum install update
+sudo yum install git
+sudo yum install docker
+git clone url
+ls
+cd 
+ls
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
+sudo docker build -t name .
+sudo docker run -d -p 8000:8000 name
+sudo docker ps
+sudo docker kill containerid
+sudo docker images
+sudo docker rmi imageid -f
+sudo docker images
+
+
+*/
